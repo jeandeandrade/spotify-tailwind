e1ae4cbfa0a5f3a2c3d21c67aa6045ae87f6d854
jeandeandrade <jeancarlos.av0@gmail.com>
2024-11-20 20:45:20 -0300

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen to music.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className=""
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
