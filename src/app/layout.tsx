c46456a6d7f0202083145872fea421877a344e3a
jeandeandrade <jeancarlos.av0@gmail.com>
2024-11-20 21:12:22 -0300

    Finished Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className=""
+        className="bg-zinc-900 text-zinc-50"
       >
         {children}
       </body>
