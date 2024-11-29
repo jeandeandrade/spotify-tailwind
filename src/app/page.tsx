1bd995d1afd09662a3038047a0e6bca61f4a5d3e
jeandeandrade <jeancarlos.av0@gmail.com>
2024-11-29 00:37:14 -0300

    Finished Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,13 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import { Footer } from '@/components/Footer';
+import { Sidebar } from '@/components/Sidebar';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume1, Maximize2 } from 'lucide-react'
 import Image from 'next/image';
 
 export default function Home() {
   return (
    <div className="h-screen flex flex-col">
     <div className="flex flex-1">
-      <aside className="w-72 bg-zinc-950 p-6">
-        <div className='flex items-center gap-2'>
-          <div className='w-3 h-3 bg-red-500 rounded-full'/>
-          <div className='w-3 h-3 bg-yellow-500 rounded-full'/>         
-          <div className='w-3 h-3 bg-green-500 rounded-full'/>
-        </div>
-        <nav className='space-y-5 mt-10'>
-          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-            <HomeIcon/>
-            Home
-          </a>
-          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-            <Search/>
-            Search
-          </a>
-          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-            <Library/>
-            Your Library
-          </a>
-        </nav>
-
-        <nav className='mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
-          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Hot Hist Brasil</a>
-          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Top Raps</a>
-          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>My Playlist #13</a>
-          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Top Brasil</a>
-        </nav>
-      </aside>
+      <Sidebar />
       <main className="flex-1 p-6">
         <div className='flex items-center gap-4'>
           <button className='rounded-full bg-black/40 p-1'>
@@ -121,9 +96,7 @@ export default function Home() {
         </div>
       </main>
     </div>
-    <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
-    </footer>
+    <Footer />
    </div>
   );
 }
